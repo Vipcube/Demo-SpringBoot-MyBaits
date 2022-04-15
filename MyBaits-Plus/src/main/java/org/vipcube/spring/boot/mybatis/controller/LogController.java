c5623cb58fb588c725175a5fb56c2e0bf3edcb70
Bread God <Vipcube@users.noreply.github.com>
2022-04-15 16:17:34 +0800

    Add log create with test create time auditing.

--- MyBaits-Plus/src/main/java/org/vipcube/spring/boot/mybatis/controller/LogController.java
+++ MyBaits-Plus/src/main/java/org/vipcube/spring/boot/mybatis/controller/LogController.java
@@ -4,8 +4,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vipcube.spring.boot.mybatis.domain.Log;
 import org.vipcube.spring.boot.mybatis.service.LogService;
 
 @RestController
@@ -15,6 +18,11 @@ public class LogController {
 
 	public LogController( LogService service ){
 		this.service = service;
+	}
+
+	@PostMapping( value = "", consumes = MediaType.APPLICATION_JSON_VALUE )
+	public ResponseEntity<?> addUser( @RequestBody Log log ){
+		return ResponseEntity.ok( this.service.save( log ) );
 	}
 
 	@GetMapping( value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE )
