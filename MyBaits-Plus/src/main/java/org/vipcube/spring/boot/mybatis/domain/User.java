76df19c2a1e353268396e6a501bc1147e6e86b19
Bread God <Vipcube@users.noreply.github.com>
2022-04-15 14:50:02 +0800

    Add mybatis generator resources.

--- MyBaits-Plus/src/main/java/org/vipcube/spring/boot/mybatis/domain/User.java
+++ MyBaits-Plus/src/main/java/org/vipcube/spring/boot/mybatis/domain/User.java
@@ -4,29 +4,30 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
- * 
+ *
  * @TableName user
  */
 @TableName(value ="user")
 @Data
 public class User implements Serializable {
     /**
-     * 
+     *
      */
     @TableId(type = IdType.AUTO)
     private Integer id;
 
     /**
-     * 
+     *
      */
     private String name;
 
     /**
-     * 
+     *
      */
     private Integer age;
 
