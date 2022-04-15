fae36883ced7dd8fc070fb70ac6a021a98da2fbf
Bread God <Vipcube@users.noreply.github.com>
2022-04-15 14:37:13 +0800

    Fixed scan package.

--- MyBaits/src/main/java/org/vipcube/spring/boot/mybatis/MyBatisApplication.java
+++ MyBaits/src/main/java/org/vipcube/spring/boot/mybatis/MyBatisApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan( {
-		"org.vipcube.spring.boot.mybatis.mapper"
+		"org.vipcube.spring.boot.mybatis.repository"
 } )
 @SpringBootApplication
 public class MyBatisApplication {
